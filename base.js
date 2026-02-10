6dd8bbf85778abe89db4ba74fc96acf5c887c7b7
abelgeorgeantony <abelgeorgeantony@gmail.com>
2026-02-10 19:31:21 +0530

    Save commit. Added dynamic horizontal separator functional

--- base.js
+++ base.js
@@ -29,43 +29,19 @@ const container = document.getElementById("figlet-container");
 /**
  * Calculates the exact character width integer
  */
-function getExactFitSize() {
-  const padding = 10; // Safety buffer for scrollbars/margins
-  const minSize = 20; // Minimum readable width (prevents requesting name1.txt)
-
+function getFittableCharacterCount(element) {
+  const padding = 26; // Safety buffer for scrollbars/margins
   // 1. Setup Canvas to measure
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
-
   // 2. MUST match your CSS exactly
-  context.font = window.getComputedStyle(
-    document.getElementById("figlet-container"),
-  ).font;
-
+  context.font = window.getComputedStyle(element).font;
   // 3. Measure one character
   const charWidth = context.measureText("X").width;
-
   // 4. Calculate raw fit
-  const rawColumns = Math.round((window.innerWidth - padding) / charWidth);
-
-  // 5. Return the calculated size, but never go below minSize
-  //return Math.max(minSize, rawColumns);
-  if (rawColumns < minSize) {
-    return 1;
-  }
-
-  console.log(rawColumns / 7);
-  const toPassToFold = Math.round(rawColumns / 7);
-  console.log(toPassToFold);
-  if (toPassToFold < 6) {
-    return toPassToFold + 4;
-  } else if (toPassToFold < 8) {
-    return toPassToFold + 5;
-  } else {
-    return toPassToFold + 6;
-  }
-  return toPassToFold;
+  return Math.round((window.innerWidth - padding) / charWidth);
 }
+
 const titleTypes = {
   name: {
     content: "Abel George Antony",
@@ -74,7 +50,22 @@ const titleTypes = {
 };
 async function fetchTitle(titleTypeName = "name") {
   const titleType = titleTypes[titleTypeName];
-  const neededSize = getExactFitSize();
+  const rawColumns = getFittableCharacterCount(
+    document.getElementById("figlet-container"),
+  );
+  const minSize = 20; // Minimum readable width (prevents requesting name1.txt)
+  if (rawColumns < minSize) {
+    return 1;
+  }
+  let neededSize = Math.round(rawColumns / 7);
+  if (neededSize < 6) {
+    neededSize = neededSize + 4;
+  } else if (neededSize < 8) {
+    neededSize = neededSize + 5;
+  } else {
+    neededSize = neededSize + 6;
+  }
+
   if (Number(neededSize) > Number(titleType.length)) {
     const fileName = Number(titleType.length) + ".txt";
     const response = await fetch(
@@ -94,12 +85,26 @@ async function fetchTitle(titleTypeName = "name") {
   }
 }
 
+function updateHorizontalSeperators() {
+  const horizontalSeperators = document.querySelectorAll(
+    ".horizontal-seperator",
+  );
+  horizontalSeperators.forEach((seperator) => {
+    const columns = getFittableCharacterCount(seperator);
+    let seperatorSymbol = seperator.dataset.separatorsymbol;
+    seperatorSymbol = seperatorSymbol.repeat(columns);
+    seperator.innerText = seperatorSymbol;
+  });
+}
+
 window.onresize = () => {
   fetchTitle();
+  updateHorizontalSeperators();
 };
 window.onload = () => {
   console.log("Page loaded");
   fetchTitle();
+  updateHorizontalSeperators();
 };
 
 function print() {
